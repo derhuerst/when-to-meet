c928b1224e541c354569e766df1f70c905620b9a
Jannis R <mail@jannisr.de>
2017-11-26 15:13:01 +0100

    read & store polls & votes

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 'use strict'
 
+const path = require('path')
+const level = require('level')
 const express = require('express')
 const hsts = require('hsts')
 const compression = require('compression')
-const path = require('path')
 const serve = require('serve-static')
 
 const showPoll = require('./routes/show-poll')
 
+const db = level(process.env.DB || './when-to-meet.ldb', {valueEncoding: 'json'})
 const app = express()
+app.locals.db = db
 
 app.use(hsts({maxAge: 24 * 60 * 60 * 1000}))
 app.use(compression())
